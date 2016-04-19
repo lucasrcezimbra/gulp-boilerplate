5e462fe380a89bef37cf40ecce50f2bb4f8f2570
Lucas Rangel Cezimbra <lucascezimbra@gmail.com>
2016-04-19 16:20:55 -0300

    Receive repository name as arg

--- git-build.sh
+++ git-build.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+if [ -z "$1" ]; then
+  echo "GitHub repository with user (example: Lrcezimbra/gulp-boilerplate): "
+  read repository
+else
+  repository="$1"
+fi
 
 gulp build
 cd build
 git init
-git remote add origin git@github.com:Lrcezimbra/REPOSITORIO.git
+git remote add origin git@github.com:"$repository".git
 git add .
 git commit -m 'Build'
 git push --force origin master:gh-pages
